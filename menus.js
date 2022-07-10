8fce7a709a7221114a87765dc09878b873b79aad
CallMeEyes <70631246+CallMeEyes@users.noreply.github.com>
2022-07-10 15:22:48 +0200

    Add files via upload

---
+++ menus.js
@@ -0,0 +1,13 @@
+var contextMenuItem = {
+    "id": "iconid",
+    "title": "Search for %s on Youtube", 
+    "contexts": ["all"], 
+};
+chrome.contextMenus.create(contextMenuItem);  
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "iconid") {
+        let newURL = ("https://www.youtube.com/results?search_query=" + (info.selectionText) + " calamity");
+        chrome.tabs.create({ url: newURL });
+    }
+});
