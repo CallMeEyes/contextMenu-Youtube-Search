1f32dd0caff00081c0ec1106a2460434555f68fc
CallMeEyes <70631246+CallMeEyes@users.noreply.github.com>
2024-02-26 14:08:23 +0100

    Update menus.js
    Made it general

--- menus.js
+++ menus.js
@@ -7,7 +7,7 @@ chrome.contextMenus.create(contextMenuItem);
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "iconid") {
-        let newURL = ("https://www.youtube.com/results?search_query=" + (info.selectionText) + " calamity");
+        let newURL = ("https://www.youtube.com/results?search_query=" + (info.selectionText));
         chrome.tabs.create({ url: newURL });
     }
 });
