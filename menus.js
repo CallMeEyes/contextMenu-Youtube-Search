9072c8f75ac192d68faa13bc109b810e6c0572cf
Duco Lam <d.f.lam@student.tudelft.nl>
2024-04-09 16:39:47 +0200

    Adds the feature to open a youtube tab as a music.youtube tab!

--- menus.js
+++ menus.js
@@ -3,11 +3,33 @@ var contextMenuItem = {
     "title": "Search for %s on Youtube", 
     "contexts": ["all"], 
 };
-chrome.contextMenus.create(contextMenuItem);  
+chrome.contextMenus.create(contextMenuItem);
+
+var contextMenuItemTWo = {
+    "id": "iconid2",
+    "title": "Open this YT video as music",
+    "contexts": ["all"],
+};
+chrome.contextMenus.create(contextMenuItemTWo);
+
+
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "iconid") {
         let newURL = ("https://www.youtube.com/results?search_query=" + (info.selectionText));
         chrome.tabs.create({ url: newURL });
     }
+    if (info.menuItemId == "iconid2") {
+        let scondurl = "hello";
+        chrome.tabs.query({'active': true, lastFocusedWindow: true}, tabs => {
+            scondurl = tabs[0].url;
+            console.log(scondurl);
+            const regex = /www/i;
+            chrome.tabs.create({ url: scondurl.replace(regex, "music")})  ;
+        }
+        );
+
+    }
 });
+
+
